7d69968fc3fa0b77c62585d350bf0764135b0186
MasukBD <imahfuz2020@outlook.com>
2024-01-03 02:22:55 +0600

    get and post api created

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
 
         const userCollection = client.db('ContactManagement').collection('users');
 
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
