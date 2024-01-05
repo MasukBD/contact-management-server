e535506f197416d08bb06848f002eaaf92d461b1
MasukBD <imahfuz2020@outlook.com>
2024-01-05 23:44:58 +0600

    intregate jwt and search query

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWToken = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized Access!' })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'Unauthorized Access!' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -26,22 +42,47 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
 
+        // Here User Stand for Contact 
         const userCollection = client.db('ContactManagement').collection('users');
+        // And Manager Stand for User 
+        const managerCollection = client.db('ContactManagement').collection('manager');
 
 
-        app.get('/users', async (req, res) => {
-            const cursor = userCollection.find();
+        app.post('/jwt', (req, res) => {
+            const client = req.body;
+            const token = jwt.sign(client, process.env.ACCESS_TOKEN, { expiresIn: '12h' })
+            res.send(token);
+        })
+
+        app.post('/manager', async (req, res) => {
+            const newUser = req.body;
+            const query = { email: newUser.email };
+            const isExistUser = await managerCollection.findOne(query);
+            if (isExistUser) {
+                return res.status(403).send({ message: 'User Exits Already!' });
+            }
+            const result = await managerCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.get('/users', verifyJWToken, async (req, res) => {
+            let query = {};
+            const phone = req.query?.search;
+            if (phone) {
+                query = { phoneNumber: { $regex: phone, $options: 'i' } }
+            }
+            const cursor = userCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.post('/users', async (req, res) => {
+        app.post('/users', verifyJWToken, async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
 
-        app.put('/users/:id', async (req, res) => {
+        app.put('/users/:id', verifyJWToken, async (req, res) => {
             const id = req.params?.id;
             const filter = { _id: new ObjectId(id) };
             const getData = req.body;
@@ -59,7 +100,7 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id', verifyJWToken, async (req, res) => {
             const id = req.params?.id;
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
